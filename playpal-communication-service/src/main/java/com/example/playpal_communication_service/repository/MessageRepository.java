44e34cee1c2b4042a0b9bb8aa90ec1ff2e265726
August Borse Haraldsen <Augustborse@gmail.com>
2024-12-05 13:17:38 +0100

    Huge overhaul of communication logic and functionality. Create conversations containing messages.

--- playpal-communication-service/src/main/java/com/example/playpal_communication_service/repository/MessageRepository.java
+++ playpal-communication-service/src/main/java/com/example/playpal_communication_service/repository/MessageRepository.java
@@ -1,9 +1,10 @@
 package com.example.playpal_communication_service.repository;
 
 import com.example.playpal_communication_service.model.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByReceiverId(Long receiverId);
+    Page<Message> findByConversationId(Long conversationId, Pageable pageable);
 }
